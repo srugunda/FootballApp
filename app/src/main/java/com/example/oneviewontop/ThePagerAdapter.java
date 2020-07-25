93ffbc8be382b1a6667be60abced25dbaa34a1cf
srugunda <srugunda@gmail.com>
2020-07-26 00:14:32 +0300

    Football App

---
+++ app/src/main/java/com/example/oneviewontop/ThePagerAdapter.java
@@ -0,0 +1,32 @@
+package com.example.oneviewontop;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ThePagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public ThePagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return new TabFragment1();
+            case 1: return new TabFragment2();
+            case 2: return new TabFragment3();
+            case 3: return new TabFragment4();
+            default: return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
